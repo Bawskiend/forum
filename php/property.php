c646486970f58c96472dd4ff17f0148e66834c7f
Bawskiend <Frayzerka@gmail.com>
2022-03-05 01:07:47 +0500

    05.03 homepc

--- php/property.php
+++ php/property.php
@@ -2,18 +2,16 @@
     session_start();
     if($_SESSION['auth'] == true){
         $connect = mysqli_connect('localhost','root','','phptest');
-        $query = "Select * from heading";
-        $result ='';
-        file_put_contents('./html/mainpage.html','');
+        $query = "Select * from property";
         $dbquery = mysqli_query($connect,$query) or die(mysqli_error($connect));
         for($data = []; $row = mysqli_fetch_assoc($dbquery); $data[] = $row);
         foreach($data as $elem){
-            $url = $elem['name'];
-            $query = "<p>" . "<a href='$url'>" . $elem['name'] . "</a>" . "</p>";
+            $query = "<h2>" . $elem['header'] . "</h2>" . "<p>" . $elem['text'] . "</p>";
             $result .= $query;
         }
-        file_put_contents('./html/mainpage.html',$result);
+        file_put_contents('./html/property.html',$result);
     }else{
-
+        $query = 'FUCK YOU';
+        file_put_contents('./html/property.html',$query);
     }
 ?>
