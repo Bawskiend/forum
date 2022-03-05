0f275e23cbdf5bbbf132bde652c7a2a5ac71a053
Румянцев Артём Игоревич <A.Rumyantsev@UKZ.LOCAL>
2022-03-05 13:58:28 +0500

    05.03 workpc

--- php/mainpage.php
+++ php/mainpage.php
@@ -3,15 +3,16 @@
     if($_SESSION['auth'] == true){
         $connect = mysqli_connect('localhost','root','','phptest');
         $query = "Select * from heading";
-        $result ='';
         file_put_contents('./html/mainpage.html','');
         $dbquery = mysqli_query($connect,$query) or die(mysqli_error($connect));
         for($data = []; $row = mysqli_fetch_assoc($dbquery); $data[] = $row);
+        $result ='<div class="list-container">';
         foreach($data as $elem){
             $url = $elem['name'];
             $query = "<p>" . "<a href='$url'>" . $elem['name'] . "</a>" . "</p>";
             $result .= $query;
         }
+        $result .='</div>';
         file_put_contents('./html/mainpage.html',$result);
     }else{
 
