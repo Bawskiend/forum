af1881fe2ed27577c9968bf6541abc92e7a9d4bd
Bawskiend <Frayzerka@gmail.com>
2022-03-07 22:32:30 +0500

    07.03 homepc

--- php/layout.php
+++ php/layout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if($_SESSION['auth'] == true){
-    $logoutlink = '<a href="/logout">Выйти</a>';
+    $logoutlink = '<a class="logout-button" href="/logout">Выйти</a>';
     $layout = str_replace('{{ header }}',$logoutlink,$layout);
 }
 elseif($_SESSION['auth'] != true and $url == '/login' ){
--- php/property.php
+++ php/property.php
@@ -4,11 +4,14 @@
         $connect = mysqli_connect('localhost','root','','phptest');
         $query = "Select * from property";
         $dbquery = mysqli_query($connect,$query) or die(mysqli_error($connect));
+        $result ='<div class="headings-container">';
+        $result .= '<p class="create-heading"><a class="create-heading-link" href="/createheading">Разместить обьявление</a></p>';
         for($data = []; $row = mysqli_fetch_assoc($dbquery); $data[] = $row);
         foreach($data as $elem){
             $query = "<h2>" . $elem['header'] . "</h2>" . "<p>" . $elem['text'] . "</p>";
             $result .= $query;
         }
+        $result .= '</div>';
         file_put_contents('./html/property.html',$result);
     }else{
        header('Location: /login');
