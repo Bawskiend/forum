de153cb403b220430edb4047e9950845a2699122
Bawskiend <Frayzerka@gmail.com>
2022-04-13 00:30:27 +0500

    13.04 homepc

--- php/mainpage.php
+++ php/mainpage.php
@@ -4,11 +4,15 @@ if($_SESSION['auth'] == true){
 echo '<div class = "create-heading"><a class = "create-heading-link" href="/addpost">Разместить статью</a></div>';
 $connect = mysqli_connect('localhost','root','','forum');
 $query = mysqli_query($connect,'Select * from topics');
+echo "<div class='posts'>";
 while($topics = mysqli_fetch_assoc($query)){
     $topic_header = $topics['header'];
-    $topic_text = $topics['text'];
-    echo "<div class='post'><a class='post-link' href=''><h2>$topic_header</h2><p>$topic_text</p></a></div>";
+    $author_login = $topics['author_login'];
+    $date = $topics['date'];
+    echo $date;
+    echo "<div class='post'><a class='post-link' href=''><h2>$topic_header</h2><p>Автор: $author_login</p><p>"?><?echo date('d.m.Y H:i', $date)?><?"</p></a></div>";
 }
+echo "</div>";
 }
 else if($_SESSION['auth'] != true){
         header('Location: /login');
