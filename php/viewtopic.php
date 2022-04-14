08837a80b57d9076a5fb144a1205cec9a4398347
Bawskiend <Frayzerka@gmail.com>
2022-04-15 00:33:06 +0500

    15.04 homepc

--- php/viewtopic.php
+++ php/viewtopic.php
@@ -6,10 +6,11 @@ if($_SESSION['auth'] == true){
     $query = "select * from topics where topic_id='$id'";
     $result = mysqli_query($connect,$query) or die(mysqli_error($connect));
     $result = mysqli_fetch_assoc($result);
+    $topic_header = $result['header'];
     if($result){
      echo "
      <div class='topic-container'>
-        <h2 class='topic_header'><h2>
+        <h2 class='topic_header'>$topic_header<h2>
      </div>";
     }
 }
