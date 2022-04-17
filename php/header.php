42d86268ecc2d54cd35ff9d5fcebc8ad5bd75817
Bawskiend <Frayzerka@gmail.com>
2022-04-18 00:46:28 +0500

    18.04 homepc

--- php/header.php
+++ php/header.php
@@ -2,12 +2,4 @@
 if($_SESSION['auth'] == true){
     echo'<a class="logout-button" href="/logout">Выйти</a>';
 }
-elseif($_SESSION['auth'] != true and $url == '/login' ){
-    echo 'Авторизация';
-}
-elseif($url== '/registration'){
-   echo ''; 
-}else{
-    echo '';
-}
 ?>
--- php/viewtopic.php
+++ php/viewtopic.php
@@ -1,22 +1,35 @@
 <?php
 session_start();
 if($_SESSION['auth'] == true){
-echo '<div class = "create-heading"><a class = "create-heading-link" href="/addpost">Разместить статью</a></div>';
     $connect = mysqli_connect('localhost','root','','forum');
     $id = $_GET['id'];
     $query = "select * from topics where topic_id='$id'";
     $result = mysqli_query($connect,$query) or die(mysqli_error($connect));
     $result = mysqli_fetch_assoc($result);
-    $topic_header = $result['header'];
-    $topic_text = $result['text'];
-    $topic_author = $result['topic_author'];
     if($result){
-     echo "
-     <div class='topic-content'>
-        <h1 class='topic-content__header'>$topic_header<h1>
+        $topic_header = $result['header'];
+        $topic_text = $result['text'];
+        $topic_author = $result['topic_author'];
+        echo "
+        <div class='topic-content'>
+        <div class = 'create-heading'><a class = 'create-heading-link' href='/addtopic'>Разместить статью</a></div>
+        <h1 class='topic-content__header'>$topic_header</h1>
         <p class='topic-content__text'>$topic_text</p>
         <p class=topic-content__author>$topic_author</p>
      </div>";
+     $query = "select * from comments where topic_id='$id' order by date";
+     $result = mysqli_query($connect,$query) or die(mysqli_error($connect));
+     $result = mysqli_fetch_assoc($result);
+     echo "<div class='comments'>";
+     if(count($result) == 0){
+     echo "<h2>В теме пока нет комментариев</h2>";
+     }
+    echo "<p>Добавьте свой комментарий</p>
+    <form method='POST' class='newcomment'>
+        <textarea name='commenttext' class='commenttext'></textarea>
+        <input type='submit' action='addcomment.php' value='Опубликовать' name='addcomment' class='addcomment'>
+     </form>
+     </div>";
+     }
     }
-}
 ?>
